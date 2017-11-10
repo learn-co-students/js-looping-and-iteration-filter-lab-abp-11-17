bbdfdba2c62e595ba6666f6ff089e0d575d16d64
Maverick Tesini <mavericktesini@gmail.com>
2017-11-10 19:47:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string) {
+  return drivers.filter(function(drivers) {
+      return drivers.toLowerCase().indexOf(string.toLowerCase()) > -1;
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(el) {
+      var fThree = string.substring(0,2);
+      if (fThree == el.substring(0,2) ) {
+        return true;
+      } else {
+        return false;
+      }
+  })
+}
+
+// filter (value, index, array)
+//scope within nested vs outside
+function matchName(collection, string) {
+  var d2 = collection.filter(function(other) {
+    debugger;
+    if (other.name === string) {
+      return true;
+    } else {
+      return false;
+    }
+  } ) ;
+  return d2;
+}
+
+/* function callMe (collection, string) {
+  debugger;
+    if (collection.name === string) {
+      return true;
+    } else {
+      return false;
+    }
+}
+*/

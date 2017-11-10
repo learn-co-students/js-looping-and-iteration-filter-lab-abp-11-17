1ed1e041070d80629466466ff32303713e28a8b6
Esther Liao <erliao@princeton.edu>
2017-11-10 17:55:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  // console.log(string);
+  // console.log(drivers);
+  return drivers.filter(function (key) { return key.toLowerCase() == string.toLowerCase()});
+  // console.log(drivers);
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (key) { return key.startsWith(string)});
+}
+
+function matchName(drivers, string){
+  console.log(string);
+  console.log(drivers);
+  console.log(drivers.string);
+  return drivers.filter(function (num) {
+    return num.name == string;
+  })
+}

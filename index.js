740f3afe65b291a03c3b7bd8869da7114bdcc780
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2017-11-11 16:49:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter(function (person) {
+    return person.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (drivers, str) {
+  const len = str.length;
+  return drivers.filter(function (person) {
+    return str === person.slice(0, len);
+  });
+}
+
+//below : drivers is an array of objects
+function matchName (drivers, str) {
+  return drivers.filter(function (person) {
+    return person.name.toLowerCase() === str.toLowerCase();
+  });
+}

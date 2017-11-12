dc57de03bb6fcbe38939471d4b851a3e4e8a959b
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-12 21:09:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+c
+
+function fuzzyMatch(list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}

6d33a60647b5f69cc518e07fed3df1713c970a10
Beth Schofield <bethmschofield@gmail.com>
2017-11-13 23:51:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  let matchingDrivers = drivers.filter(function(name) {
+    return name.toUpperCase() === string.toUpperCase();
+  })
+  return matchingDrivers
+}
+
+function fuzzyMatch(drivers, letter) {
+  let matchingFirstLetter = drivers.filter(function(name) {
+    return name.startsWith(letter);
+  })
+return matchingFirstLetter
+}
+
+
+// function matchName(drivers, string) {
+//   let matchingNames = drivers.filter(function(drivername) {
+//     return drivers["name"] === string;
+//   })
+// return matchingNames;
+// }
+
+function matchName(drivers, string) {
+  let matchedNames = drivers.filter(function(driver){
+      // return drivers["name"] === drivername;
+      return driver["name"].toUpperCase() === string.toUpperCase();
+    });
+return matchedNames;
+}

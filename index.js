d44cb8836c9a89ee6090c30d1d4f6c9d5e57d077
Arren Alexander <aralx73@gmail.com>
2017-11-14 20:21:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file
+
+
+
+function findMatching (arrayOfDrivers, name) {
+    let matchingDrivers = arrayOfDrivers.filter(function(driverName) {
+      if (name.toLowerCase() === driverName.toLowerCase()){
+        return true;
+      } else {
+         return false;
+        }
+    })
+    return matchingDrivers;
+}
+
+
+function fuzzyMatch(drivers, letters) {
+  let nameLength = letters.length;
+
+  let fuzzyDrivers = drivers.filter(function(driverName) {
+    if (letters === driverName.slice(0, nameLength)) {
+        return true;
+    } else {
+        return false;
+    }
+  })
+  return fuzzyDrivers;
+}
+
+
+function matchName(drivers, name) {
+  let nameMatch = drivers.filter (function (driver){
+      if (driver.name.toLowerCase() === name.toLowerCase()) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return nameMatch;
+}

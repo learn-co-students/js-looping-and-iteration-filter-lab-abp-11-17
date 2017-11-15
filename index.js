a2decfacaa9d0b34655172f575324eaa1a36ce7b
Tim Shipman <bricklayertim@yahoo.com>
2017-11-15 18:18:52 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function findMatching(drivers, name){
   })
   return matchingDrivers;
 }
+
+function fuzzyMatch(drivers, firstLetter){
+  let wholeName = firstLetter.length;
+  return drivers.filter(function(driverName){
+    return driverName.slice(0, wholeName) === firstLetter;
+  })
+}
+
+function matchName(driver, name){
+  return driver.filter(function(list){
+    return list.name.toLowerCase() === name.toLowerCase();
+  })
+}

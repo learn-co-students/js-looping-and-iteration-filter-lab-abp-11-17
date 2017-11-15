1bd40f86a8c2195236df61062594e1897b06fb2c
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-15 17:46:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file
+
+//returns all drivers that match the passed in name
+
+
+//ePenguinUsers = filter(users, function (user) { return user.favoriteColor === 'Blue' && user.favoriteAnimal === 'Penguin';
+function findMatching(drivers, name) {
+  let matchingDrivers = drivers.filter(function(driver){
+     if (name.toLowerCase() === driver.toLowerCase()){
+       return true
+     } else {
+       return false
+     }
+})
+
+return matchingDrivers;
+}
+
+
+
+function fuzzyMatch(drivers, name) {
+
+  let firstLetters = drivers.filter(function(driver){
+    if(name[0].toLowerCase() === driver[0].toLowerCase()){
+      return true
+    } else {
+      return false
+    }
+
+
+})
+
+return firstLetters;
+}
+
+function matchName(drivers, name) {
+let matchy = drivers.filter(function(driver){
+    if(name === driver.name){
+      return true
+    } else {
+      return false
+    }
+
+
+})
+return matchy;
+}

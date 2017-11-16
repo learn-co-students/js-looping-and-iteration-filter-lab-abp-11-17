9555791485e669ea4319d509ad7167bf0bb327b6
jamieberrier <berrier.jamie@gmail.com>
2017-11-16 21:47:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+  return drivers.filter(function (name){
+    return name===string || name===string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string){
+  let lengthOfName = string.length;
+  return drivers.filter(function(name){
+    return name.slice(0, lengthOfName)===string;
+  });
+}
+
+function matchName(drivers, string){
+  let matchedNames = drivers.filter(function(driver){
+    return driver['name'] === string;
+  });
+return matchedNames;
+}

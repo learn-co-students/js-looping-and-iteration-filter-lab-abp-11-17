3154e1a2272296cc34d34501f1137f91fc15f8af
Sarah Tennis <sarah.n.tennis@gmail.com>
2017-11-17 03:57:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file
+function findMatching(driversArray, myString){
+  return driversArray.filter(function(value){
+    value = value.toUpperCase();
+    myString = myString.toUpperCase();
+    return value === myString}
+  )
+}
+
+function fuzzyMatch(driversArray, myString){
+  return driversArray.filter(function(value){
+    let firstLetterValue = value[0].toUpperCase();
+    let firstLetterMyString = myString[0].toUpperCase();
+    return firstLetterMyString === firstLetterValue
+  })
+}
+
+function matchName(drivers, string){
+  return drivers.filter(function(value){
+    return value.name === string
+  })
+}

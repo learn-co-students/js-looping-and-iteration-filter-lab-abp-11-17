c5ddf38a1359cc5118f08063471a1b555a013fd7
Adil Asif <asifadilanis@gmail.com>
2017-11-18 20:35:18 -0800

    Done.

--- index.js
+++ index.js
@@ -2,20 +2,6 @@ function findMatching(drivers, name) {
   return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
 }
 
-// function fuzzyMatch(drivers, partialName) {
-//   const result = [];
-//   for (const driverName of drivers) {
-//     let nameChars = '';
-//     for (const ch of driverName) {
-//       nameChars += ch;
-//       if (nameChars === partialName) {
-//         result.push(driverName);
-//       }
-//     }
-//   }
-//   return result;
-// }
-
 function fuzzyMatch(drivers, name) {
   return drivers.filter(driver => driver.slice(0, name.length) === name);
 }

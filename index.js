74092f24e4eb30ecf7f52234da2991024eddcd84
Leslie Stevens <flylady@mail.utexas.edu>
2017-11-22 23:34:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+  }
+  function fuzzyMatch(drivers, firstLetters) {
+    let nameLength = firstLetters.length;
+    return drivers.filter(function (driverBeginning) {
+      return driverBeginning.slice(0, nameLength) === firstLetters;
+    });
+  }
+  function matchName(drivers, nameProperty) {
+    return drivers.filter(function (guess) {
+      return guess.name.toLowerCase() === nameProperty.toLowerCase();
+    })
+  }

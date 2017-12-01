db52b979aac9487ed835d21460a5d5f1d3959021
Dean Stigall <stigallrealestate@gmail.com>
2017-12-01 00:55:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file
+
+function findMatching(drivers, index, name){
+  let newDrivers = drivers.filter(function(drivers, index, name){
+    return index === name.indexOf(drivers);
+  });
+}

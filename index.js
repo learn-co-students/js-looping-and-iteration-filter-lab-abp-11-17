62ebbf638bfb2d0153765282769c54ccc57ff773
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-01-11 20:37:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+ return drivers.filter(function(driverName) {
+    return driverName.toLowerCase() === string.toLowerCase();
+  })
+}
+
+function fuzzyMatch(drivers , string) {
+  return drivers.filter(function (letterMatch){
+    return letterMatch.charAt(0) === string.charAt(0);
+  })
+}
+
+function matchName(drivers, string) {
+ return drivers.filter(function(pablo) {
+    return pablo.name === string;
+  });
+}

02707b72b9c23eecb728f447223f1f08e74cdea3
carlitos321 <ca.cabrera321@gmail.com>
2018-01-15 23:49:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+
+}
+
+function fuzzyMatch(drivers, string) {
+
+}
+
+function matchName(drivers, string) {
+
+}

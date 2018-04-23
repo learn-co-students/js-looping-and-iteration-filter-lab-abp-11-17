c7949d96bc8a9a04e3e408f67d78b9511e64fb86
Eric Kim <hyeokjungkim@gmail.com>
2018-04-23 15:08:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(arr, str){
+  return arr.filter(function (element) {return element.toLowerCase() === str.toLowerCase()})
+}
+
+function fuzzyMatch(arr, str){
+  return arr.filter(function (element) {return element.startsWith(str)})
+}
+
+function matchName(arr, str){
+  return arr.filter(function(element) {return element.name === str})
+}
